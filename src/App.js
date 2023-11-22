fe322480fa04dbef563c7bd53300004418a9eb69
Hritik Arora <hritikarora@hritiks-mbp.home>
2023-11-22 11:02:03 -0400

    React Structure

--- src/App.js
+++ src/App.js
@@ -1,5 +1,9 @@
 import logo from './logo.svg';
 import './App.css';
+import Stack from 'react-bootstrap/Stack';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
 
 function App() {
   return (
@@ -17,6 +21,12 @@ function App() {
         >
           Learn React
         </a>
+        <Stack direction="horizontal" gap={3}>
+      <div className="p-2">First item</div>
+      <div className="p-2 ms-auto">Second item</div>
+      <div className="vr" />
+      <div className="p-2">Third item</div>
+    </Stack>
       </header>
     </div>
   );
